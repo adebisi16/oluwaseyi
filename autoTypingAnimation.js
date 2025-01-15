794275eb04f2e7e919ea8b02557e5c9638c1a837
adebisi16 <155408734+adebisi16@users.noreply.github.com>
2025-01-15 10:00:27 +0100

    Add files via upload

---
+++ autoTypingAnimation.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Adebisi Oluwaseyi </span> I'm a Web Developer with 5 years of experience, specializing in creating fully responsive websites and handling backend development. Additionally, I have expertise in crafting 2D and 3D animations to enhance user experiences. "],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
